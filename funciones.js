6159460de47564a682955bd5b8a3f03422d24be8
Alejandro Ontiveros <31458536+gandroide@users.noreply.github.com>
2017-08-30 09:31:38 -0400

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -9,3 +9,8 @@ var saludo = function (nombre) {
 
 //¡En la línea 11, llama a la función de saludo!
 
+
+
+//Este es el ejercicio que me tiene vienco para los lados jajajaja
+//lo que ocurre es que no entiendo bien aun lo de las funciones
+//o como llamarlas xD please help!!!

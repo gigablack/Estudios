5640c09b4575d9caabcc1f22df48bd6036b49158
gandroide <31458536+gandroide@users.noreply.github.com>
2017-08-30 07:30:21 -0400

    Ejercicio de Codecademy
    Practicas para entender el funcionamiento de las funciones.

---
+++ funciones.js
@@ -0,0 +1,11 @@
+//A continuación está la función de saludo
+//Observa la línea 7
+//Podemos unir strings usando el signo de más +
+//Para saber más, consulta la sugerencia
+
+var saludo = function (nombre) {
+    console.log("Qué bueno verte," + " " + nombre);
+};
+
+//¡En la línea 11, llama a la función de saludo!
+
